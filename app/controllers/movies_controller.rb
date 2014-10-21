c521e9f5609aa532fa4080a14477da80f1a5d324
mengqi68 <mengqi68@gmail.com>
2014-10-21 08:10:06 -0400

    Update movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,14 +1,48 @@
-# This file is app/controllers/movies_controller.rb
 class MoviesController < ApplicationController
-
-  def index
-    @movies = Movie.all
-  end
 
   def show
     id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # Look up movie by unique ID
+    @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
+  end
+
+  def index
+    @movies = Movie.all
+    @all_ratings = Movie.ratings
+    @sort = params[:sort]
+    @selectedRatings = params[:ratings]
+
+    if (@sort!=nil && @selectedRatings!={})
+	flash.keep
+	@movies =  Movie.where(:rating => @selectedRatings.keys).order(@sort + ' ASC')
+    end
+ 
+    if @selectedRatings!=nil
+	    session[:savedRatings] = @selectedRatings
+	    if (session[:savedSortVal]!=nil)
+	    	@movies = Movie.where(:rating => @selectedRatings.keys).order(session[:savedSortVal] + ' ASC')
+	    else
+		@movies = Movie.where(:rating => @selectedRatings.keys)
+	    end
+    else
+	@selectedRatings = {}
+    end
+
+    if @sort!=nil
+	session[:savedSortVal] = @sort
+	if @selectedRatings!={}
+	    @movies = Movie.where(:rating => @selectedRatings.keys).order(@sort + ' ASC')
+	else
+	    @movies = Movie.order(@sort + ' ASC')
+	end
+    end
+
+    if (@sort==nil && @selectedRatings=={} && (session[:savedSortVal]!=nil or session[:savedRatings]!=nil))
+	flash.keep
+	redirect_to movies_path(:sort => session[:savedSortVal], :ratings => session[:savedRatings])
+    end
+
+    @movies
   end
 
   def new
